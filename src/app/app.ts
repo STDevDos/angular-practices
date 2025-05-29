a99f9e162793fdb7dae30c2699f1356c16cd13b3
froyaguilar <fdaf58@gmail.com>
2025-05-28 21:11:37 -0600

    commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,26 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'] // ✅ must be "styleUrls" (array)
 })
+
 export class App {
-  protected title = 'angular-practices';
+  title: string = 'angular-practices';
+  name: string = 'Froylan Aguilar';
+
+  year: number = 2025;                 // ✔️ Clear
+  isVisible: boolean = true;           // ✔️ Clear
+
+  //we can include variable as a functions, but no inside the class
+  hello(): void {
+    let x: number = 10;
+    const y = 20;
+    var z: number = 30;
+    console.log(x, y, z);
+  }
+
+  helloV2(): string {
+    return `Hello ${this.name}, your title is ${this.title}`;
+  }
+
 }
